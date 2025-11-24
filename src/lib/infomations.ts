55e9da5cb8fa5f7e154451af3fb458a3adc0e19d
Shankar Dutta <shankardutta1719@gmail.com>
2025-11-24 09:24:09 +0530

    Update Information & Type file
    add social media icons and data in infomation.ts .
    
    add social media icons type in type.ts .

--- src/lib/infomations.ts
+++ src/lib/infomations.ts
@@ -1,4 +1,7 @@
-import { NavItemsType } from "./type";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiGithub, FiInstagram } from "react-icons/fi";
+import { ImFacebook } from "react-icons/im";
+import { NavItemsType, SocialMediaIconsType } from "./type";
 
 export const navItems: NavItemsType = [
 	{ id: 1, label: "Home", urlid: { pathname: "/" } },
@@ -16,5 +19,32 @@ export const navItems: NavItemsType = [
 		id: 4,
 		label: "Contact",
 		urlid: { pathname: "contact" },
+	},
+];
+
+export const socialMediaIcons: SocialMediaIconsType = [
+	{
+		id: 1,
+		icons: ImFacebook,
+		link: "/", // add your facebook account link
+		ariaName: "Facebook-icon",
+	},
+	{
+		id: 2,
+		icons: FiInstagram,
+		link: "/", // add your insta account Link
+		ariaName: "Instagram-icon",
+	},
+	{
+		id: 3,
+		icons: FiGithub,
+		link: "/", // add your github account link
+		ariaName: "github-icon",
+	},
+	{
+		id: 4,
+		icons: FaLinkedinIn,
+		link: "/", // add your linkedin account link
+		ariaName: "linkedin-icon",
 	},
 ];
